dbbf156b92b225df67199a9a7cae1f21cdc8b6a4
Nairi <st146151@student.spbu.ru>
2025-10-13 11:56:21 +0000

    Optimizing the script

--- script_smallest.py
+++ script_smallest.py
@@ -1,8 +1,10 @@
-import two_smallest
+from two_smallest import find_two_smallest
 import lists
-my_file="result.txt"
-file=open(my_file, 'w')
-file.write(str(two_smallest.find_two_smallest(lists.list_1))+"\n")
-file.write(str(two_smallest.find_two_smallest(lists.list_2))+"\n")
-file.write(str(two_smallest.find_two_smallest(lists.list_3))+"\n")
+
+my_file = "result.txt"
+file = open(my_file, 'w')
+
+file.write(str(find_two_smallest(lists.list_1)) + "\n")
+file.write(str(find_two_smallest(lists.list_2)) + "\n")
+file.write(str(find_two_smallest(lists.list_3)) + "\n")
 file.close()
