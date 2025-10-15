3e4852742823d688ccb947dd6d7412e1c0da9e46
Anastasia <ay.golovacheva@gmail.com>
2025-10-15 18:53:15 +0000

    Change two_smallest

--- two_smallest.py
+++ two_smallest.py
@@ -3,6 +3,6 @@ def find_two_smallest(int_list):
     This function find and returns 
     two smallest list values
     """
-    sorted_list = int_list
+    sorted_list = list.copy(int_list)
     sorted_list.sort()
     return(sorted_list[0:2])
